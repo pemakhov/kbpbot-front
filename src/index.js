d05b257e584df9842e2d496c6e6fa615bea60a7b
pemakhov <serhiy.pemakhov@gmail.com>
2021-06-28 18:59:37 +0300

    refactor: bootstrap
    import bootstrap.bundle.min

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
